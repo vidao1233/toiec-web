f73f5d429e862d4bb922da24a8216ced3f0d4ba0
Lê Quang Duẩn <20110619@student.hcmute.edu.vn>
2023-12-04 10:06:44 +0700

    "update vocbytopicid api"

--- toiec-web/ViewModels/Vocabulary/VocabularyViewModel.cs
+++ toiec-web/ViewModels/Vocabulary/VocabularyViewModel.cs
@@ -3,6 +3,7 @@
     public class VocabularyViewModel
     {
         public Guid idTopic { get; set; }
+        public Guid idVoc { get; set; } 
         public string engWord { get; set; }
         public string wordType { get; set; }
         public string meaning { get; set; }
