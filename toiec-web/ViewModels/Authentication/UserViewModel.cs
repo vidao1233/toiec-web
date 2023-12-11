8af70f7a1d733b30bcfd31516aef0c7f23ed7609
vidao1233 <vidao1233@gmail.com>
2023-12-11 22:46:20 +0700

    update date of birth of user

--- toiec-web/ViewModels/Authentication/UserViewModel.cs
+++ toiec-web/ViewModels/Authentication/UserViewModel.cs
@@ -1,13 +1,17 @@
-﻿namespace toiec_web.ViewModels.User
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace toiec_web.ViewModels.User
 {
     public class UserViewModel
     {
         public string fullname { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
         public string dateOfBirth { get; set; }
         public bool gender { get; set; }
         public string username { get; set; }
         public string email { get; set; }
-        public string phone { get; set; }
+        public string phonenumber { get; set; }
         public string imageURL { get; set; }
     }
 }
